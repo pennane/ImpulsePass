ee8b065d8bae919405d5341caccb1bb2df6ccda3
Arttu Pennanen <arttu@pennanen.org>
2023-02-19 19:36:35 +0200

    add test suite & update related configs

---
+++ test/kide/KideAppApiTest.java
@@ -0,0 +1,29 @@
+package kide;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class KideAppApiTest {
+	KideAppApi api;
+
+	@BeforeEach
+	void setup() {
+		api = new KideAppApi();
+	}
+
+	@Test
+	@DisplayName("It should fetch events from kide.app api")
+	void productsTest() {
+		Optional<List<KideAppEvent>> optionalEvents = api.fetchEvents();
+		assertTrue(optionalEvents.isPresent());
+		assertFalse(optionalEvents.get().isEmpty());
+	}
+
+}
